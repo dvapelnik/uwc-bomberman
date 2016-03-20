79cfe4b4d75e8310deabc416527a69d0cd73242e
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-20 14:46:19 +0200

    Move test refactored

--- tests/Bomberman/PlayerTest.js
+++ tests/Bomberman/PlayerTest.js
@@ -7,53 +7,66 @@ var
 
 describe('Player', function () {
     [
-        {direction: 'right', prev: [0, 0], next: [1, 0], active: 0, shouldMove: true},
-        {direction: 'down', prev: [0, 0], next: [0, 1], active: 0, shouldMove: true},
-        {direction: 'down', prev: [12, 0], next: [12, 1], active: 1, shouldMove: true},
-        {direction: 'left', prev: [12, 0], next: [11, 0], active: 1, shouldMove: true},
-        {direction: 'right', prev: [0, 10], next: [1, 10], active: 2, shouldMove: true},
-        {direction: 'up', prev: [0, 10], next: [0, 9], active: 2, shouldMove: true},
-        {direction: 'up', prev: [12, 10], next: [12, 9], active: 3, shouldMove: true},
-        {direction: 'left', prev: [12, 10], next: [11, 10], active: 3, shouldMove: true}
+        {direction: 'right', prev: [0, 0], next: [1, 0], shouldMove: true},
+        {direction: 'down', prev: [0, 0], next: [0, 1], shouldMove: true},
+        {direction: 'left', prev: [0, 0], next: [-1, 0], shouldMove: false},
+        {direction: 'up', prev: [0, 0], next: [0, -11], shouldMove: false},
+
+        {direction: 'down', prev: [12, 0], next: [12, 1], shouldMove: true},
+        {direction: 'left', prev: [12, 0], next: [11, 0], shouldMove: true},
+        {direction: 'up', prev: [12, 0], next: [12, -1], shouldMove: false},
+        {direction: 'right', prev: [12, 0], next: [13, 0], shouldMove: false},
+
+        {direction: 'right', prev: [0, 10], next: [1, 10], shouldMove: true},
+        {direction: 'up', prev: [0, 10], next: [0, 9], shouldMove: true},
+        {direction: 'left', prev: [0, 10], next: [-1, 10], shouldMove: false},
+        {direction: 'down', prev: [0, 10], next: [0, 11], shouldMove: false},
+
+        {direction: 'up', prev: [12, 10], next: [12, 9], shouldMove: true},
+        {direction: 'left', prev: [12, 10], next: [11, 10], shouldMove: true},
+        {direction: 'down', prev: [12, 10], next: [12, 11], shouldMove: false},
+        {direction: 'right', prev: [12, 10], next: [13, 10], shouldMove: false}
     ].map(function (option) {
         var
             direction = option.direction
             , prev = option.prev
             , next = option.next
-            , active = option.active
             ;
 
-        var
-            place = new Place()
-            , players = makePlayersWithActiveAt(active)
-            ;
-
-        expect(players.all).to.be.not.undefined;
-        expect(players.active).to.be.not.undefined;
-
-        place.buildPlace(10);
-        place.setPlayers(players.all);
+        var placeAndPlayer = getPlaceWithPlayerAt(prev[0], prev[1])
+            , place = placeAndPlayer.place
+            , player = placeAndPlayer.player;
 
         it('should ' + (option.shouldMove ? '' : 'not ') + 'move to ' + option.direction, function () {
             expect(place.place[prev[1]][prev[0]]).to.be.an.instanceof(Player);
-            expect(place.place[next[1]][next[0]]).to.be.null;
 
             var
-                oldLocation = _.clone(players.active.location)
-                , locations = players.active.move(direction)
-                , newLocation = _.clone(players.active.location);
+                oldLocation = _.clone(player.location)
+                , locations = player.move(direction)
+                , newLocation = _.clone(player.location);
 
             expect(locations.old).to.be.not.undefined;
             expect(locations.current).to.be.not.undefined;
-            expect(locations.old.x).to.be.equal(oldLocation.x);
-            expect(locations.old.y).to.be.equal(oldLocation.y);
-            expect(locations.current.x).to.be.equal(newLocation.x);
-            expect(locations.current.y).to.be.equal(newLocation.y);
-            expect(locations.current.x).to.be.equal(next[0]);
-            expect(locations.current.y).to.be.equal(next[1]);
 
-            expect(place.place[prev[1]][prev[0]]).to.be.null;
-            expect(place.place[next[1]][next[0]]).to.be.instanceof(Player);
+            if (option.shouldMove) {
+                expect(locations.old.x).to.be.equal(oldLocation.x);
+                expect(locations.old.y).to.be.equal(oldLocation.y);
+                expect(locations.current.x).to.be.equal(newLocation.x);
+                expect(locations.current.y).to.be.equal(newLocation.y);
+                expect(locations.current.x).to.be.equal(next[0]);
+                expect(locations.current.y).to.be.equal(next[1]);
+
+                expect(place.place[prev[1]][prev[0]]).to.be.null;
+                expect(place.place[next[1]][next[0]]).to.be.instanceof(Player);
+            } else {
+                expect(oldLocation.x).to.be.equal(locations.old.x);
+                expect(oldLocation.y).to.be.equal(locations.old.y);
+
+                expect(oldLocation.x).to.be.equal(locations.current.x);
+                expect(oldLocation.y).to.be.equal(locations.current.y);
+            }
+
+
         });
     });
 });
@@ -71,4 +84,22 @@ function makePlayersWithActiveAt(activeIndex) {
     }
 
     return playersResult;
+}
+
+function getPlaceWithPlayerAt(x, y) {
+    var
+        place = new Place()
+        , player = new Player()
+        ;
+
+    place.buildPlace(10);
+    place.place[y][x] = player;
+    player.location = {x: x, y: y};
+    player.place = place;
+    player.active = true;
+
+    return {
+        place: place,
+        player: player
+    }
 }
